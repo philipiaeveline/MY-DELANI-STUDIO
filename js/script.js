dda04742640660195a3c87b6b34eedd2d17dcee8
philipiaeveline <philipiaeveline@gmail.com>
2020-10-09 15:44:55 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+$(".eve").click(function() {
+    $(".eve").hide(2000)
+    $(".eve1").show(2000);
+});
+$(".eve1").click(function() {
+    $(".eve1").hide(2000)
+    $(".eve").show(2000);
+});
+});
+
+$(document).ready(function() {
+    $(".hope").click(function() {
+        $(".hope").hide(2000)
+        $(".hope1").show(2000);
+    });
+    $(".hope1").click(function() {
+        $(".hope1").hide(2000)
+        $(".hope").show(2000);
+    });
+    });
