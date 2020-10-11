969d49b776a98e221eb88bd9e2e0dfff3a08d1df
philipiaeveline <philipiaeveline@gmail.com>
2020-10-11 12:13:43 +0300

    add

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(".eve").click(function() {
     $(".eve").hide(2000)
     $(".eve1").show(2000);
 });
+
 $(".eve1").click(function() {
     $(".eve1").hide(2000)
     $(".eve").show(2000);
@@ -18,11 +19,11 @@ $(document).ready(function() {
         $(".hope1").hide(2000)
         $(".hope").show(2000);
     });
-    });
+});
 
-    $(document).ready(function() {
-        $(".cat").click(function() {
-            $(".cat").hide(2000)
+$(document).ready(function() {
+    $(".cat").click(function() {
+          $(".cat").hide(2000)
             $(".cat1").show(2000);
         });
         $(".cat1").click(function() {
@@ -36,3 +37,27 @@ $(document).ready(function() {
         }
 
 
+$("#img1").hover(function(){
+    $(".log1").toggle();
+})
+$("#img2").hover(function(){
+    $(".log2").toggle();
+})
+$("#img3").hover(function(){
+    $(".log3").toggle();
+})
+$("#img4").hover(function(){
+    $(".log4").toggle();
+})
+$("#img5").hover(function(){
+    $(".log5").toggle();
+})
+$("#img6").hover(function(){
+    $(".log6").toggle();
+})
+$("#img7").hover(function(){
+    $(".log7").toggle();
+})
+$("#img8").hover(function(){
+    $(".log8").toggle();
+})
